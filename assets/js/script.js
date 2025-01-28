2a6035669fb16c7904170aed48aaccd9ff1f2b5e
unknown <robertomallorca18@gmail.com>
2025-01-28 11:25:26 +0800

    Updated index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,3 +87,63 @@ $(function() {
     });
     
 });
+
+
+const certifications = [
+    'assets/img/cert1.png',
+    'assets/img/cert2.png',
+    'assets/img/cert3.png',
+    'assets/img/cert4.png',
+    'assets/img/cert5.png',
+    'assets/img/cert6.png',
+    'assets/img/cert7.png',
+    'assets/img/cert8.png',
+    'assets/img/cert9.png',
+    'assets/img/cert10.png',
+    'assets/img/cert11.png'
+];
+
+let currentCertificationIndex = 0;
+
+function showCertification(index) {
+    currentCertificationIndex = index;
+    document.getElementById('certificationImage').src = certifications[index];
+    $('#certificationModal').modal('show');
+}
+
+document.getElementById('prevCertification').addEventListener('click', function() {
+    if (currentCertificationIndex > 0) {
+        showCertification(currentCertificationIndex - 1);
+    } else {
+        showCertification(certifications.length - 1);
+    }
+});
+
+document.getElementById('nextCertification').addEventListener('click', function() {
+    if (currentCertificationIndex < certifications.length - 1) {
+        showCertification(currentCertificationIndex + 1);
+    } else {
+        showCertification(0);
+    }
+});
+
+document.querySelectorAll('[data-target="#certificationModal"]').forEach((element, index) => {
+    element.addEventListener('click', function() {
+        showCertification(index);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const heroTitle = document.getElementById('hero-title');
+    heroTitle.addEventListener('animationend', function() {
+        heroTitle.classList.remove('typing');
+        const heroContent = document.getElementById('hero-content');
+        const delayedElements = heroContent.querySelectorAll('.delayed-fade-in');
+        delayedElements.forEach((element, index) => {
+            element.style.animation = `fadeIn 2s ease-in ${index * 0.5}s forwards`;
+        });
+        // Start .mypic animation after typing effect
+        const myPic = document.querySelector('.mypic');
+        myPic.style.animationPlayState = 'running';
+    });
+});
